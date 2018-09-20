26caac32ec24387251a441ea68cd91678cdcc4f9
João Marcos <j-m-lima@hotmail.com>
2018-09-20 12:24:00 -0300

    Add solution for problem `B - Relatively Prime Pairs` Link: https://codeforces.com/contest/1051/problem/B Tags: greedy, math, number theory, *1000 Ref: https://codeforces.com/contest/1051/submission/43130304

---
+++ codeforces/1051/B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define trace1(a) cout << a << '\n';
+#define trace2(a, b) cout << a << ' ' << b << '\n';
+#define trace3(a, b, c) cout << a << ' ' << b << ' ' << c << '\n';
+#define trace4(a, b, c, d) cout << a << ' ' << b << ' ' << c << ' ' << d << '\n';
+#define trace5(a, b, c, d, e) cout << a << ' ' << b << ' ' << c << ' ' << d << ' ' << e << '\n';
+#define trace6(a, b, c, d, e, f) cout << a << ' ' << b << ' ' << c << ' ' << d << ' ' << e << ' ' << f << '\n';
+#define printArr(harry, tam) range(tam) cout << harry[i] << " \n"[i == tam -1];
+#define range(n) for(int i = 0; i < n; i++)
+#define maxn 2000000
+#define mod 1000000007
+#define md(x) (x) % mod;
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define prt(x) cout << (#x) << " is " << (x) << endl
+#define EPS 1e-9
+#define INF 1000000000
+#define INFd 1e9
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<double> vd;
+typedef pair<int, vector<int> > piv;
+ll n, m, q, k, t, a, b, c, x, y, z, w;
+char s[100];
+char ch;
+int main() {
+	sc("%I64d %I64d \n", &a, &b);
+	pr("YES\n");
+	for(ll i = a; i < b; i+= 2ll) {
+		pr("%I64d %I64d\n", i, i+1);
+	}
+}
