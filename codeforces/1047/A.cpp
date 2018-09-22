8f8f4835896940678398510af1789e28d80d472e
João Marcos <j-m-lima@hotmail.com>
2018-09-22 15:54:00 -0300

    Add solution for problem `A - Little C Loves 3 I` Link: https://codeforces.com/contest/1047/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1047/submission/43270989

---
+++ codeforces/1047/A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define trace1(a) cout << a << '\n';
+#define trace2(a, b) cout << a << ' ' << b << '\n';
+#define trace3(a, b, c) cout << a << ' ' << b << ' ' << c << '\n';
+#define trace4(a, b, c, d) cout << a << ' ' << b << ' ' << c << ' ' << d << '\n';
+#define trace5(a, b, c, d, e) cout << a << ' ' << b << ' ' << c << ' ' << d << ' ' << e << '\n';
+#define trace6(a, b, c, d, e, f) cout << a << ' ' << b << ' ' << c << ' ' << d << ' ' << e << ' ' << f << '\n';
+#define printArr(harry, tam) range(tam) cout << harry[i] << " \n"[i == tam -1];
+#define range(n) for(int i = 0; i < n; i++)
+#define maxn 2000000
+#define mod 1000000007
+#define md(x) (x) % mod;
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define ps push
+#define prt(x) cout << (#x) << " is " << (x) << endl
+#define EPS 1e-9
+#define INF 1000000000
+#define INFd 1e9
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<double> vd;
+typedef pair<int, vector<int> > piv;
+int val[8];
+int n, m, a, b, c, d;
+int fin = 0, ini = 0;
+int n1 = 0, n2 = 0;
+int li[200000];
+int zero = 0;
+int main(){
+  sc("%d\n", &n);
+  if (n % 3 == 0) {
+    pr("1 1 %d", n-2);
+  } else if (n % 3 == 1) {
+    pr("1 1 %d", n-2);
+  } else  {
+    pr("1 2 %d", n-3);
+  }
+  return 0;
+}
