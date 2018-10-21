71930b30217dd08de4f63cfbb05975ee00cc0283
João Marcos <j-m-lima@hotmail.com>
2018-10-21 05:18:00 -0300

    Add solution for problem `A - Golden Plate` Link: https://codeforces.com/contest/1072/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1072/submission/44626292

---
+++ codeforces/1072/A.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define printArr(harry, tam) range(tam) cout << harry[i] << " \n"[i == tam -1];
+#define range(n) for(int i = 0; i < n; i++)
+#define maxn 100000
+#define mod 1000000007
+#define md(x) ((x) % mod)
+#define sc scanf
+#define pr printf
+#define getc getchar
+#define putc putchar
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define prt(x) cout << (#x) << " is " << (x) << endl
+#define EPS 1e-9
+#define INF INT_MAX
+#define INFd 1e9
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef vector<double> vd;
+
+//Trace dinamico
+template<typename T>
+void trace(T a) { cout << a << "\n"; }
+template<typename T, typename... Args>
+void trace(T a, Args... args) { cout << a << " "; trace(args...); }
+
+//DP debug
+int recNum = 0;
+void prTabs() { range(recNum) cout << "    "; }
+template<typename... Args>
+void recInit(Args... args) { prTabs(); trace("rec(", args..., ")"); recNum++; }
+template<typename T>
+void recEnd(T a) { prTabs(); trace("->", a); recNum--;}
+int n, m, a, b, c;
+//vector<int> g[maxn]; 
+
+int main(){
+    sc("%d %d %d\n", &a, &b, &c);
+    int resp = 0;
+    while (a >= 3 && b >= 3 && c >= 1) {
+        resp += a + a + b + b;
+        resp -= 4;
+        a -= 4;
+        b -= 4;
+        c--;
+    }
+    pr("%d\n", resp);
+
+	/*
+	//Entrada 1
+    while ((ch = getc()) != '\n') {
+      s[n++] = ch;
+    }
+	//Entrada 2
+    sc("%d\n", &n);
+    for (int i = 0; i < n; i++) {
+        sc("%d", li+i);
+    }
+	//Entrada 3
+    sc("%s", &s);
+	//Entrada 4
+    while(sc("%d\n", &n) != EOF){
+
+    }*/
+ 	return 0;
+}
