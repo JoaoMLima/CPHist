9b2c748435b6b77718136a4f54d7c963ccec3aa3
João Marcos <j-m-lima@hotmail.com>
2018-11-25 13:41:00 -0300

    Add solution for problem `A - Determine Line` Link: https://codeforces.com/contest/1056/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1056/submission/46210959

---
+++ codeforces/1056/A.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define printArr(harry, tam) range(tam) cout << harry[i] << " \n"[i == tam -1];
+#define range(n) for(int i = 0; i < n; i++)
+#define maxn 100000
+#define mod 1000000007
+#define md(x) ((((x) % mod) + mod) % mod)
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define prt(x) cout << (#x) << " is " << (x) << endl
+#define EPS 1e-9
+#define INF INT_MAX
+#define INFd 1e9
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+//Trace dinamico
+template<typename T>
+void trace(T a) { cout << a << "\n"; }
+template<typename T, typename... Args>
+void trace(T a, Args... args) { cout << a << " "; trace(args...); }
+
+//DP debug
+int recNum = 0;
+void prTabs() { range(recNum) cout << "    "; }
+template<typename... Args>
+void recInit(Args... args) { prTabs(); trace("rec(", args..., ")"); recNum++; }
+template<typename T>
+void recEnd(T a) { prTabs(); trace("->", a); recNum--;}
+int n, m, a, b, c;
+//vector<int> g[maxn]; 
+int li[101];
+
+int main(){
+    memset(li, 0, sizeof(li));
+    sc("%d\n", &n);
+    for(int i = 0; i < n; i++) {
+        sc("%d", &m);
+        for(int j = 0; j < m; j++) {
+            sc("%d", &a);
+            li[a]++;
+        }
+    }
+    bool pri = false;
+    for(int i = 0; i < 101; i++) {
+        if (li[i] == n) {
+            if(pri) {pr(" ");}
+            pr("%d", i);
+            pri = true;
+        }
+    }
+    pr("\n");
+	/*
+	//Entrada 1
+    while ((ch = getc()) != '\n') {
+      s[n++] = ch;
+    }
+	//Entrada 2
+    sc("%d\n", &n);
+    for (int i = 0; i < n; i++) {
+        sc("%d", li+i);
+    }
+	//Entrada 3
+    sc("%s", &s);
+	//Entrada 4
+    while(sc("%d\n", &n) != EOF){
+
+    }*/
+ 	return 0;
+}
