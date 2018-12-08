626309461ee00d59bc41d18d1bfd728133a66401
João Marcos <j-m-lima@hotmail.com>
2018-12-08 00:39:00 -0300

    Add solution for problem `B - Buying a TV Set` Link: https://codeforces.com/contest/1041/problem/B Tags: math, *1000 Ref: https://codeforces.com/contest/1041/submission/46741909

---
+++ codeforces/1041/B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define printArr(harry, tam) range(tam) cout << harry[i] << " \n"[i == tam -1];
+#define range(n) for(int i = 0; i < n; i++)
+#define maxn 100000
+#define mod 1000000007
+#define md(x) ((((x) % mod) + mod) % mod)
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define prt(x) cout << (#x) << " is " << (x) << endl
+#define EPS 1e-9
+#define INF INT_MAX
+#define INFd 1e9
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+//Trace dinamico
+template<typename T>
+void trace(T a) { cout << a << "\n"; }
+template<typename T, typename... Args>
+void trace(T a, Args... args) { cout << a << " "; trace(args...); }
+
+//DP debug
+int recNum = 0;
+void prTabs() { range(recNum) cout << "    "; }
+template<typename... Args>
+void recInit(Args... args) { prTabs(); trace("rec(", args..., ")"); recNum++; }
+template<typename T>
+void recEnd(T a) { prTabs(); trace("->", a); recNum--;}
+ll n, m, a, b, c, d;
+//vector<int> g[maxn]; 
+
+ll gcd(ll a, ll b) {
+    return (b == 0)? a : gcd(b, a % b);
+}
+
+int main(){
+	sc("%lld %lld %lld %lld\n", &a, &b, &c, &d);
+    ll g = gcd(c, d);
+    c /= g; d /= g;
+    pr("%lld\n", min(a/c, b/d));
+ 	return 0;
+}
