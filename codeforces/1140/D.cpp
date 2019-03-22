c1f7fad095d5be5a07db4198ec738d01b6d16fed
João Marcos <j-m-lima@hotmail.com>
2019-03-22 13:14:00 -0300

    Add solution for problem `D - Minimum Triangulation` Link: https://codeforces.com/contest/1140/problem/D Tags: dp, greedy, math, *1200 Ref: https://codeforces.com/contest/1140/submission/51708685

---
+++ codeforces/1140/D.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define printArr(harry, tam) range(tam) cout << harry[i] << " \n"[i == tam -1];
+#define range(n) for(int i = 0; i < n; i++)
+#define maxn 100000
+#define mod 1000000007
+#define md(x) ((((x) % mod) + mod) % mod)
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define prt(x) cout << (#x) << " is " << (x) << endl
+#define EPS 1e-9
+#define INF INT_MAX
+#define INFd 1e9
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+//Trace dinamico
+template<typename T>
+void trace(T a) { cout << a << "\n"; }
+template<typename T, typename... Args>
+void trace(T a, Args... args) { cout << a << " "; trace(args...); }
+
+//DP debug
+int recNum = 0;
+void prTabs() { range(recNum) cout << "    "; }
+template<typename... Args>
+void recInit(Args... args) { prTabs(); trace("rec(", args..., ")"); recNum++; }
+template<typename T>
+void recEnd(T a) { prTabs(); trace("->", a); recNum--;}
+int n, m, a, b, c;
+//vector<int> g[maxn]; 
+
+int main(){
+	sc("%d", &n);
+    ll mini = (1LL)<<60;
+    for(int i = 0; i < n; i++) {
+        ll sum = 0LL;
+        for(int j = 1; j < n-1; j++) {
+            int i2 = (j+i) % n;
+            int i3 = (j+i+1) % n;
+            sum += ((i+1) * (i2+1) *(i3+1))+0LL;
+        }
+        mini = min(mini, sum);
+    }
+    pr("%lld\n", mini);
+ 	return 0;
+}
