47312162f300a701d50400eaa72a1fc0f3b0f79b
João Marcos <j-m-lima@hotmail.com>
2020-03-22 16:51:00 -0300

    Add solution for problem `B - Maximum Value` Link: https://codeforces.com/contest/484/problem/B Tags: binary search, math, sortings, two pointers, *2100 Ref: https://codeforces.com/contest/484/submission/73991647

--- codeforces/484/B.cpp
+++ codeforces/484/B.cpp
@@ -12,9 +12,13 @@ typedef pair<int, int> ii;
 
 int ult[1000001];
 int tem[1000001];
-int passou[2000001];
+int pre[2000010];
+int nex[2000010];
 
-
+void erase(int x) {
+	nex[pre[x]] = nex[x];
+	pre[nex[x]] = pre[x];
+}
 int ultimo(int x) {
 	if (x > 1000000) return ult[1000000];
 	return ult[x-1];
@@ -24,12 +28,17 @@ int main(){
 	int n, a;
 	vector<int> v;
 	sc("%d", &n);
+	for(int i = 0; i <= 2000001; i++) {
+		pre[i] = i-1;
+		nex[i] = i+1;
+	}
 	for(int i = 0; i < n; i++) {
 		sc("%d", &a);
 		v.pb(a);
 		tem[a] = 1;
 	}
 
+	sort(v.begin(), v.end(), greater<int>());
 	int atual = 0;
 	for(int i = 1; i <= 1000000; i++) {
 		if (tem[i]) atual = i;
@@ -37,13 +46,16 @@ int main(){
 	}
 	int maxi = 0;
 
-	sort(v.begin(), v.end(), greater<int>());
-
 	for(int x : v) {
-		if (passou[x]) continue;
-		for(int i = x; i <= 2000000; i += x) {
-			if (ultimo(i) >= x) maxi = max(maxi, ultimo(i)%x);
-			passou[i] = 1;
+		if (pre[nex[x]] != x) continue;
+		//cout << x << endl;
+		int i = nex[0];
+		while(i != 2000001) {
+			//cout << "i: " << i << endl;
+			if ((i*1LL*x) > 2000000LL) break;
+			if (ultimo(i*x) >= x) maxi = max(maxi, ultimo(i*x)%x);
+			erase(i*x);
+			i = nex[i];
 		}
 	}
 	pr("%d\n", maxi);
