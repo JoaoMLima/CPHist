108a37a14f30358f2e69c730ee4d693f9e6015be
João Marcos <j-m-lima@hotmail.com>
2020-05-04 19:36:00 -0300

    Add solution for problem `A - Filling Diamonds` Link: https://codeforces.com/contest/1339/problem/A Tags: brute force, dp, implementation, math, *900 Ref: https://codeforces.com/contest/1339/submission/79008551

---
+++ codeforces/1339/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+int n, a, b, c;
+int main(){
+	int t;
+	sc("%d", &t);
+	while(t--) {
+		sc("%d", &n);
+		pr("%d\n", n);
+	}
+ 	return 0;
+}
