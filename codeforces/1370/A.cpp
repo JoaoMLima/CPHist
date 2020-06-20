75d1f1af1b098f784b22ed20bf0f9a2528155052
João Marcos <j-m-lima@hotmail.com>
2020-06-20 11:37:00 -0300

    Add solution for problem `A - Maximum GCD` Link: https://codeforces.com/contest/1370/problem/A Tags: greedy, implementation, math, number theory, *800 Ref: https://codeforces.com/contest/1370/submission/84425443

---
+++ codeforces/1370/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define MAX 100010
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+int n, t, a, b, c;
+int main(){
+	sc("%d", &t);
+	while(t--) {
+		sc("%d", &n);
+		pr("%d\n", n/2);
+	}
+ 	return 0;
+}
