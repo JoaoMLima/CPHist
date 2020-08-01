19c358c1bdba4e3050acfe482a64b00a32214121
João Marcos <j-m-lima@hotmail.com>
2020-08-01 10:14:00 -0300

    Add solution for problem `C - Mafia` Link: https://codeforces.com/contest/349/problem/C Tags: implementation, *1600 Ref: https://codeforces.com/contest/349/submission/88666975

---
+++ codeforces/349/C.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define MAX 1000007
+#define MOD 1000000007
+#define MODL 1000000007LL
+#define sz(x) ((int)(x).size())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+
+// Trace dinâmico
+template<typename T>
+void trace(T a) { cout << a << "\n"; }
+template<typename T, typename... Args>
+void trace(T a, Args... args) { cout << a << " "; trace(args...); }
+
+// Debugador de recursão
+int recNum = 0;
+void prTabs() { for (int ii = 0; ii < recNum; ++ii) cout << "    "; }
+template<typename... Args>
+void recInit(Args... args) { prTabs(); trace("rec(", args..., ")"); recNum++; }
+template<typename T>
+void recEnd(T a) { prTabs(); trace("->", a); recNum--;}
+
+int MD(int x) {
+	if(x < 0) return x+MOD;
+	if(x >= MOD) return x-MOD;
+	return x;
+}
+
+ll MDL(ll x) {
+	return (x%MODL+MODL)%MODL;
+}
+
+ll modpow(ll a, ll b, ll m) {
+	if (!b) return 1LL;
+	if (b&1LL) return a*modpow(a, b-1, m)%m;
+	ll r = modpow(a, b>>1, m);
+	return r*r%m;
+}
+
+//m prime
+ll invmod(ll a , ll m) {
+	return modpow(a, m-2, m);
+}
+
+ll li[MAX];
+
+int n, t, a, b, c;
+int main(){
+	sc("%d", &n);
+	for(int i = 1; i <= n; i++) {
+		sc("%lld", li+i);
+	}
+	ll l = 1, r = 1000000000000000LL;
+	ll resp = r;
+	while(l <= r) {
+		ll m = (l+r)/2LL;
+		ll total = 0LL;
+		int ok = 1;
+		for(int i = 1; i <= n; i++) {
+			if (m < li[i]) {
+				ok = 0;
+			}
+		}
+		if (ok != 0) {
+			for(int i = 1; i <= n; i++) {
+				total += m-li[i];
+				if (total >= m) break;
+			}
+		}
+		if (!ok || total < m) {
+			l = m+1;
+		} else {
+			resp = m;
+			r = m-1;
+		}
+	}
+	pr("%lld\n", resp);
+
+ 	return 0;
+}
