a90f421e4ad1b2a890d15d4189aa2a96600e4103
João Marcos <j-m-lima@hotmail.com>
2020-08-09 11:40:00 -0300

    Add solution for problem `A - Suborrays` Link: https://codeforces.com/contest/1391/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1391/submission/89412570

---
+++ codeforces/1391/A.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define MAX 100007
+#define MOD 1000000007
+#define MODL 1000000007LL
+#define sz(x) ((int)(x).size())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+
+// Trace dinâmico
+template<typename T>
+void trace(T a) { cout << a << "\n"; }
+template<typename T, typename... Args>
+void trace(T a, Args... args) { cout << a << " "; trace(args...); }
+
+// Debugador de recursão
+int recNum = 0;
+void prTabs() { for (int ii = 0; ii < recNum; ++ii) cout << "    "; }
+template<typename... Args>
+void recInit(Args... args) { prTabs(); trace("rec(", args..., ")"); recNum++; }
+template<typename T>
+void recEnd(T a) { prTabs(); trace("->", a); recNum--;}
+
+int MD(int x) {
+	if(x < 0) return x+MOD;
+	if(x >= MOD) return x-MOD;
+	return x;
+}
+
+ll MDL(ll x) {
+	return (x%MODL+MODL)%MODL;
+}
+
+ll modpow(ll a, ll b, ll m) {
+	if (!b) return 1LL;
+	if (b&1LL) return a*modpow(a, b-1, m)%m;
+	ll r = modpow(a, b>>1, m);
+	return r*r%m;
+}
+
+//m prime
+ll invmod(ll a , ll m) {
+	return modpow(a, m-2, m);
+}
+
+
+
+int n, t, a, b, c;
+int main(){
+	sc("%d", &n);
+	/*
+	for(int i = 1; i <= 100; i++) {
+		for(int j = i; j <= 100; j++) {
+			int ou = 0;
+			int ok = 1;
+			for(int k = i; k <= j; k++) {
+				ou |= k;
+				if (ou  < k-i+1) {
+					ok = 0;
+				}
+			}
+			if (ok == 0) pr("%d %d\n", i, j);
+		}
+	}*/
+	for(int i = 0; i < n; i++) {
+		sc("%d", &a);
+		for(int j = 1; j <= a; j++) {
+			if (j > 1) pr(" ");
+			pr("%d", j);
+		}pr("\n");
+	}
+ 	return 0;
+}
