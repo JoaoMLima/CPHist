660c492451cd9611ead6d4735a518a3d856a27ba
João Marcos <j-m-lima@hotmail.com>
2020-09-29 17:42:00 -0300

    Add solution for problem `A - LCM Problem` Link: https://codeforces.com/contest/1389/problem/A Tags: constructive algorithms, greedy, math, number theory, *800 Ref: https://codeforces.com/contest/1389/submission/94246438

---
+++ codeforces/1389/A.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define MAX 100007
+#define MOD 1000000007
+#define MODL 1000000007LL
+#define EPS 1e-12
+#define sz(x) ((int)(x).size())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll, ll> ii;
+
+// Trace dinâmico
+template<typename T>
+void trace(T a) { cout << a << "\n"; }
+template<typename T, typename... Args>
+void trace(T a, Args... args) { cout << a << " "; trace(args...); }
+
+// Debugador de recursão
+int recNum = 0;
+void prTabs() { for (int ii = 0; ii < recNum; ++ii) cout << "    "; }
+template<typename... Args>
+void recInit(Args... args) { prTabs(); trace("rec(", args..., ")"); recNum++; }
+template<typename T>
+void recEnd(T a) { prTabs(); trace("->", a); recNum--;}
+
+ll modpow(ll a, ll b, ll m) {
+	if (!b) return 1LL;
+	if (b&1LL) return a*modpow(a, b-1, m)%m;
+	ll r = modpow(a, b>>1, m);
+	return r*r%m;
+}
+
+
+int crivo[1007];
+int n, a, b;
+string s;
+vector<int> v;
+int main(){
+	sc("%d", &n);
+	while(n--) {
+		sc("%d %d", &a, &b);
+		if (a*2 <= b) pr("%d %d\n", a, a*2);
+		else pr("-1 -1\n");
+	}
+	
+ 	return 0;
+}
