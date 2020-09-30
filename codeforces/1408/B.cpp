703d90308514e592d7d89b7168ce62f77e565f9d
João Marcos <j-m-lima@hotmail.com>
2020-09-30 14:49:00 -0300

    Add solution for problem `B - Arrays Sum` Link: https://codeforces.com/contest/1408/problem/B Tags: constructive algorithms, greedy, math, *1400 Ref: https://codeforces.com/contest/1408/submission/94348421

---
+++ codeforces/1408/B.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define MAX 100007
+#define MOD 1000000007
+#define MODL 1000000007LL
+#define EPS 1e-12
+#define sz(x) ((int)(x).size())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+
+// Trace dinâmico
+template<typename T>
+void trace(T a) { cout << a << "\n"; }
+template<typename T, typename... Args>
+void trace(T a, Args... args) { cout << a << " "; trace(args...); }
+
+// Debugador de recursão
+int recNum = 0;
+void prTabs() { for (int ii = 0; ii < recNum; ++ii) cout << "    "; }
+template<typename... Args>
+void recInit(Args... args) { prTabs(); trace("rec(", args..., ")"); recNum++; }
+template<typename T>
+void recEnd(T a) { prTabs(); trace("->", a); recNum--;}
+
+int MD(int x) {
+	if(x < 0) return x+MOD;
+	if(x >= MOD) return x-MOD;
+	return x;
+}
+
+ll MDL(ll x) {
+	return (x%MODL+MODL)%MODL;
+}
+
+ll modpow(ll a, ll b, ll m) {
+	if (!b) return 1LL;
+	if (b&1LL) return a*modpow(a, b-1, m)%m;
+	ll r = modpow(a, b>>1, m);
+	return r*r%m;
+}
+
+//m prime
+ll invmod(ll a , ll m) {
+	return modpow(a, m-2, m);
+}
+
+int li[107];
+
+int n, t, k, a, b, c;
+int main(){
+	sc("%d\n", &t);
+	while(t--) {
+		sc("%d %d", &n, &k);
+		int difs = 0;
+		for(int i = 0; i < n; i++) {
+			sc("%d", li+i);
+			if (i && li[i] != li[i-1]) {
+				difs++;
+			}
+		}
+		if (!difs) {
+		    pr("1\n");
+		} else if (k == 1) {
+			pr("-1\n");
+		} else {
+			pr("%d\n", difs/(k-1) + ((difs%(k-1))? 1 : 0));
+		}
+	}
+ 	return 0;
+}
