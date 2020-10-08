9312c000517edd8a67be61123bac91c6f8bba3e7
João Marcos <j-m-lima@hotmail.com>
2020-10-08 14:23:00 -0300

    Add solution for problem `C - The Fair Nut and String` Link: https://codeforces.com/contest/1084/problem/C Tags: combinatorics, dp, implementation, *1500 Ref: https://codeforces.com/contest/1084/submission/94934674

---
+++ codeforces/1084/C.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define MAX 100007
+#define MOD 1000000007
+#define MODL 1000000007LL
+#define EPS 1e-12
+#define sz(x) ((int)(x).size())
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+
+// Trace dinâmico
+template<typename T>
+void trace(T a) { cout << a << "\n"; }
+template<typename T, typename... Args>
+void trace(T a, Args... args) { cout << a << " "; trace(args...); }
+
+// Debugador de recursão
+int recNum = 0;
+void prTabs() { for (int ii = 0; ii < recNum; ++ii) cout << "    "; }
+template<typename... Args>
+void recInit(Args... args) { prTabs(); trace("rec(", args..., ")"); recNum++; }
+template<typename T>
+void recEnd(T a) { prTabs(); trace("->", a); recNum--;}
+
+int MD(int x) {
+	if(x < 0) return x+MOD;
+	if(x >= MOD) return x-MOD;
+	return x;
+}
+
+ll MDL(ll x) {
+	return (x%MODL+MODL)%MODL;
+}
+
+ll modpow(ll a, ll b, ll m) {
+	if (!b) return 1LL;
+	if (b&1LL) return a*modpow(a, b-1, m)%m;
+	ll r = modpow(a, b>>1, m);
+	return r*r%m;
+}
+
+//m prime
+ll invmod(ll a , ll m) {
+	return modpow(a, m-2, m);
+}
+
+
+
+int n, t, a, b;
+int main(){
+	string s;
+	cin >> s;
+	ll res = 1LL;
+	ll cnt = 1LL;
+	for(char c : s) {
+		if(c=='a')cnt++;
+		else if(c=='b'){res = MDL(res*cnt);cnt = 1LL;}
+	}
+	res = MDL(res*cnt);
+	pr("%lld\n", MDL(res-1LL+MODL));
+ 	return 0;
+}
