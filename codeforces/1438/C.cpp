ea057bfa6593d1156c2cefb04c8feb9caba572a7
João Marcos <j-m-lima@hotmail.com>
2020-11-13 11:55:00 -0300

    Add solution for problem `C - Engineer Artem` Link: https://codeforces.com/contest/1438/problem/C Tags: 2-sat, chinese remainder theorem, constructive algorithms, fft, flows, *2000 Ref: https://codeforces.com/contest/1438/submission/98296686

---
+++ codeforces/1438/C.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sc scanf
+#define pr printf
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define MAX 100007
+#define MOD 1000000007
+#define MODL 1000000007LL
+#define EPS 1e-12
+#define sz(x) ((int)(x).size())
+#define L(x) ((x)*2+1)
+#define R(x) ((x)*2+2)
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+
+
+#ifdef EBUG
+#include "trace.h"
+#else
+template<typename... Args>
+void trace(Args... args) {}
+template<typename... Args>
+void recInit(Args... args) {}
+template<typename... Args>
+void recEnd(Args... args) {}
+#endif
+
+int MD(int x) {
+	if(x < 0) return x+MOD;
+	if(x >= MOD) return x-MOD;
+	return x;
+}
+
+ll MDL(ll x) {
+	return (x%MODL+MODL)%MODL;
+}
+
+ll modpow(ll a, ll b, ll m) {
+	if (!b) return 1LL;
+	if (b&1LL) return a*modpow(a, b-1, m)%m;
+	ll r = modpow(a, b>>1, m);
+	return r*r%m;
+}
+
+//m prime
+ll invmod(ll a , ll m) {
+	return modpow(a, m-2, m);
+}
+
+
+
+int n, t, a, m, b, c;
+int main(){
+    sc("%d", &t);
+	while(t--) {
+		sc("%d %d", &n, &m);
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				sc("%d", &a);
+				if (j) pr(" ");
+				pr("%d", a + (a+i+j)%2);
+			}pr("\n");
+		}
+	}
+
+ 	return 0;
+}
